ae962699383efcc1d5caedf3d7e4376cfac3c549
pad19 <pad19051999@gmail.com>
2020-07-02 13:14:38 +0300

    Add task 5

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 'use strict';
 
-const sqrt = Math.sqrt(245);
+const arr = [4, 2, 5, 19, 13, 0, 10];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += Math.pow(i, 3);
+}
+const result = Math.sqrt(sum);
