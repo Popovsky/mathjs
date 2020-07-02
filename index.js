3d449be0e6546934ad2b060457bbfc11ed52621e
pad19 <pad19051999@gmail.com>
2020-07-02 13:42:02 +0300

    Add task 9

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
 
-const arrOfNum = [4, -2, 5, 19, -130, 0, 10];
-const minNum = Math.min(...arrOfNum);
-const maxNum = Math.max(...arrOfNum);
+const intNum = Math.ceil(Math.random() * 100);
+alert(intNum);
