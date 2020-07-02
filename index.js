7e8b0758777416b4752a6777fa58c2d511b72ed0
pad19 <pad19051999@gmail.com>
2020-07-02 13:08:11 +0300

    Add task 3

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 'use strict';
 
-const a = Number(prompt('Enter the number'));
-const b = Number(prompt('Enter the number'));
-
-if (a % b) {
-    console.log(`Делится с остатком: ${a} % ${b} = ${a % b}`);
-} else console.log(`Делится: ${a} / ${b} = ${a / b}`);
+const st = Math.pow(2, 10);
