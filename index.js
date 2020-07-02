f63fef2f1e7865b17c5b6383404d98c91f7ad987
pad19 <pad19051999@gmail.com>
2020-07-02 13:30:19 +0300

    Add task 7

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const num = 379;
-const roundToInt = Number(Math.sqrt(num).toFixed());
-const roundToTenths = Number(Math.sqrt(num).toFixed(1));
-const roundToHundredths = Number(Math.sqrt(num).toFixed(2));
+const num = 587;
+const obj = {
+    floor: Math.floor(Math.sqrt(num)),
+    ceil: Math.ceil(Math.sqrt(num)),
+};
